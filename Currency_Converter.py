2e65faa15c70d661b5c7f759cb7cab0e9067277a
Nambativ Peter Nambativ <116292003+Nambativep@users.noreply.github.com>
2023-12-05 15:54:04 +0100

    Add files via upload
    Importance is attached here for using an software application for converting currency for easier understanding of the differences in exchange rate

---
+++ Currency_Converter.py
@@ -0,0 +1,20 @@
+# create a function called "main()"
+# Convert Naira to USD, BPS
+# show the user the answer
+# use Lambda function to add the converte
+
+
+def convert_to_pounds(dollars):
+    pass
+
+
+    def main():
+        print("This program to converts dollars to BPS ")
+        print()
+        dollars = eval(input("Enter Amount in USD: "))
+
+        pounds = convert_to_pounds(dollars)
+
+        convert_to_pounds = lambda dollars: dollars * 0.82
+
+        print("This is", pounds, "pounds")
